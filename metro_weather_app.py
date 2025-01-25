bb7cce8f6def6b0baaad3889a243bcbdfca10e36
Francen-Red <francenvenissered@gmail.com>
2025-01-26 04:27:34 +0800

    Added a dictionary named color codes for text colors

--- metro_weather_app.py
+++ metro_weather_app.py
@@ -1,8 +1,22 @@
 import requests
 
-ncr_cities = [ "caloocan", "calabon", "navotas", "valenzuela", "quezon city",
+# Create a list of cities in Metro Manila
+ncr_cities = [ "caloocan", "malabon", "navotas", "valenzuela", "quezon city",
     "marikina", "pasig", "taguig", "makati", "manila",
     "mandaluyong", "san juan", "pasay", "parañaque", "las piñas", "muntinlupa"]
+
+# Create a dictionary of text colors using ANSII color codes 
+color_codes = {
+    "red": "\033[31m",
+    "orange": "\033[38;5;214m",
+    "yellow": "\033[33m",
+    "green": "\033[32m",
+    "blue": "\033[34m",
+    "pink": "\033[38;5;213m",
+    "violet": "\033[35m",
+    "brown": "\033[38;5;94m",
+    "reset_color": "\033[0m"
+}
 
 def fetch_weather_data(city_name):
     weather_url = "https://api.openweathermap.org/data/2.5/weather?"  # base URL to fetch data from website
@@ -41,15 +55,15 @@ def display_weather(data):
     wind_speed = data["wind"]["speed"]
 
     # Display the weather information
-    print(f"\nWeather in {city}:")
-    print(f"Temperature: {temp}°C")
-    print(f"Condition: {weather_description.capitalize()}")
-    print(f"Humidity: {humidity}%")
-    print(f"Wind Speed: {wind_speed} m/s")
+    print(f"{color_codes['red']}\nWeather in {city}:{color_codes['reset_color']}")
+    print(f"{color_codes['orange']}Temperature:{color_codes['reset_color']} {temp}°C")
+    print(f"{color_codes['yellow']}Condition:{color_codes['reset_color']} {weather_description.capitalize()}")
+    print(f"{color_codes['green']}Humidity:{color_codes['reset_color']} {humidity}%")
+    print(f"{color_codes['blue']}Wind Speed:{color_codes['reset_color']} {wind_speed} m/s")
 
 while True:
     # Ask the user for a city
-    city_name = input("Hi! Please input a city in NCR, Philippines: ").lower()
+    city_name = input(f"{color_codes['brown']}Hi! Please input a city in NCR, Philippines:{color_codes['reset_color']} ").lower()
     if city_name not in ncr_cities:  # Check if the city is valid
         print("Please input a city in NCR, Philippines only.")
         continue 
@@ -60,7 +74,7 @@ while True:
 
     # Ask the user if they want to input another city
     while True:
-        another_city = input("Do you want to input another city? (yes/no): ").strip().lower()
+        another_city = input(f"{color_codes['pink']}Do you want to input another city? (yes/no):{color_codes['reset_color']} ").strip().lower()
         if another_city == "yes":
             break  # Restart the main loop
         elif another_city == "no":
