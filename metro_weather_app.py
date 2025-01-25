45a78fef71e71e55aa16943a859d6c2236297e19
Francen-Red <francenvenissered@gmail.com>
2025-01-25 13:45:21 +0800

    Defined a function to fetch weather data, with base URL and reading of the API key from a file.

--- metro_weather_app.py
+++ metro_weather_app.py
@@ -4,9 +4,14 @@ ncr_cities = [ "Caloocan", "Malabon", "Navotas", "Valenzuela", "Quezon City",
     "Marikina", "Pasig", "Taguig", "Makati", "Manila",
     "Mandaluyong", "San Juan", "Pasay", "Parañaque", "Las Piñas", "Muntinlupa"]
 
-while True:
-    # Ask the user for a city
-    city_name = input("Hi! Please input a city in NCR, Philippines: ")
-    if city_name not in ncr_cities:  # Ask 
-        print("Please input a city in NCR, Philippines only.")
-        continue 
+def fetch_weather_data(city_name):
+    weather_url = "https://api.openweathermap.org/data/2.5/weather?"  # base URL to fetch data from website
+    with open("api_key", "r") as file:
+            api_key = file.read().strip()
+
+    while True:
+        # Ask the user for a city
+        city_name = input("Hi! Please input a city in NCR, Philippines: ")
+        if city_name not in ncr_cities:  # Check if the city is valid
+            print("Please input a city in NCR, Philippines only.")
+            continue 
